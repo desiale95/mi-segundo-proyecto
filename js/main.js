2529dce825913d0441c32cb83b29e4da7104ce08
desiale95 <desiale95@gmail.com>
2023-07-01 13:23:42 -0300

    mi tercer commit proyecto 2

--- js/main.js
+++ js/main.js
@@ -26,39 +26,3 @@ toggle.addEventListener('change',(event)=>{
             
         }
 });
-
-var x=0;
-
-function cambioImagen(){
-    if(x==0){
-        document.getElementById("imgchange").src="img/imagen2.jpg";
-        x++
-    }else{
-        document.getElementById("imgchange").src="img/imagen1.jpg";
-        x=0
-    }
-}
-
-const $tiempo = document.querySelector('.tiempo'), $fecha = document.querySelector('.fecha');
-
-function digitalClock(){
-    let f = new Date(),
-    dia = f.getDate(),
-    mes = f.getMonth() + 1,
-    anio = f.getFullYear(),
-    diaSemana = f.getDay();
-
-    dia = ('0' + dia).slice(-2);
-    mes = ('0' + mes).slice(-2)
-
-    let timeString = f.toLocaleTimeString();
-    $tiempo.innerHTML = timeString;
-    
-    let semana = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
-    let showSemana = (semana[diaSemana]);
-    $fecha.innerHTML = `${anio}-${mes}-${dia}-${showSemana}`
-}
-
-setInterval(() => {
-    digitalClock()
-}, 1000);
